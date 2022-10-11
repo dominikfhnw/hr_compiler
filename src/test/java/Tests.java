51042961a7b49ae53dce72a337372ff4c206a56e
Yasmin <yasminhrdina@hotmail.com>
2022-10-11 17:40:35 +0200

    implemented JUnit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,22 +1,20 @@
 import errors.LexicalError;
+import org.junit.Test;
 import scanner.Scanner;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Test {
+public class Tests {
 
-    public static void main(String[] args) {
+    @Test
+    public void testScanner() {
         String file = "src/main/resources/grossmutter.txt";
-        if(args.length > 0){
-            file = args[0];
-        }
         Path filePath = Path.of(file);
         System.out.println(filePath.toAbsolutePath());
-        String content;
         try {
-            content = Files.readString(filePath);
+            String content = Files.readString(filePath);
             Scanner.scan(content);
         } catch (IOException | LexicalError e) {
             e.printStackTrace();
