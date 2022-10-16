711cbfadf91374ca5e2358cb48bb1c063ac17b19
Yasmin <yasminhrdina@hotmail.com>
2022-10-16 13:56:04 +0200

    use implemented methods in switch-case

--- src/main/java/scanner/Scanner.java
+++ src/main/java/scanner/Scanner.java
@@ -3,17 +3,13 @@ package scanner;
 import errors.LexicalError;
 import scanner.enums.Operators;
 import scanner.enums.Terminals;
+import scanner.interfaces.IToken;
+
 import java.util.Arrays;
 public class Scanner {
 
     public static void debug(String msg){
         System.out.println(msg);
-    }
-
-    public static boolean isSpecial(char c){
-        return Arrays.stream(Terminals.values()).anyMatch(terminals -> terminals.getSymbol() == c) ||
-            Arrays.stream(Operators.values()).anyMatch(operators -> operators.getSymbol() == c);
-        // return Arrays.asList('(', ',', ')', ':', ';', '=', '/', '\\', '/', '<', '>', '+', '-', '*', '~', '!', '?').contains(c); // has been replaced by line 14/15
     }
 
     public static ITokenList scan(CharSequence cs) throws LexicalError {
@@ -23,14 +19,14 @@ public class Scanner {
         ITokenList list = new ITokenList();
         int state = 0;
         StringBuffer lexAccu = null; // for constructing the identifier
-        // StringBuffer symAccu = null; // for constructing the special symbol
+        StringBuffer symAccu = null; // for constructing the special symbol
         long numAccu = 0L; // for constructing the literal value
 
         for (int i = 0; i < cs.length(); i++) {
             char c = cs.charAt(i);
             // debug("char " + c);
             switch (state) {
-                case 0:
+                case 0: // state 0 = scan next token
                     if (Character.isDigit(c)) {
                         state = 2;
                         numAccu = Character.digit(c, 10);
@@ -42,7 +38,7 @@ public class Scanner {
                         //debug("0 -> 1 alphabetic " + lexAccu);
                     } else if (isSpecial(c)) {
                         state = 3;
-                        // symAccu = new StringBuffer();
+                        // symAccu = new StringBuffer(1024);
                         // symAccu.append(c);
                         lexAccu = new StringBuffer(1024);
                         lexAccu.append(c);
@@ -52,17 +48,21 @@ public class Scanner {
                         throw new LexicalError("illegal char: " + c);
                     }
                     break;
-                case 1:
+                case 1: // state 1 = scan identifier
                     if (Character.isLetterOrDigit(c)){
                         //debug("1 digit " + lexAccu);
                         lexAccu.append(c);
                     } else {
                         state = 0;
                         i = i - 1;
+                        if(isKeyword(String.valueOf(lexAccu))){
+                            IToken token = Terminals.valueOf(String.valueOf(lexAccu));
+                            list.add(token);
+                        }
                         debug("IDENT " + lexAccu);
                     }
                     break;
-                case 2:
+                case 2: // state 2 = scan literal
                     if (Character.isDigit(c)) {
                         int digit = Character.digit(c, 10);
                         numAccu = numAccu * 10 + digit;
@@ -77,13 +77,20 @@ public class Scanner {
                         list.add(new Literal(Terminals.LITERAL, (int) numAccu));
                     }
                     break;
-                case 3:
+                case 3: // state 3 = scan symbol
+                    //if (isSubsequentSymbol(c, symAccu)){
+                    //state = 0;
+                    //symAccu.append(c);
+                    //IToken token = Terminals.valueOf(String.valueOf(symAccu));
+                    //list.add(token);
                     if (isSpecial(c)){
                         lexAccu.append(c);
                     } else {
                         state = 0;
                         i = i - 1;
                         debug("OP " + lexAccu);
+                        // IToken token = Terminals.valueOf(String.valueOf(symAccu));
+                        // list.add(token);
                     }
                     break;
                 default:
@@ -95,19 +102,22 @@ public class Scanner {
         return list;
     }
 
-    public boolean isKeyword(String lexeme){
-        return Arrays.stream(Terminals.values()).anyMatch(terminals -> terminals.getLexeme().equals(lexeme)) ||
-            Arrays.stream(Operators.values()).anyMatch(operators -> operators.getLexeme().equals(lexeme));
-    }
-
-    private boolean isSymbol(char c) {
+    public static boolean isSpecial(char c){
         return Arrays.stream(Terminals.values()).anyMatch(terminals -> terminals.getSymbol() == c) ||
             Arrays.stream(Operators.values()).anyMatch(operators -> operators.getSymbol() == c);
     }
 
+    public static boolean isKeyword(String lexeme){
+        return Arrays.stream(Terminals.values()).anyMatch(terminals -> terminals.getLexeme().equals(lexeme)) ||
+            Arrays.stream(Operators.values()).anyMatch(operators -> operators.getLexeme().equals(lexeme));
+    }
+
     // checks if char is a subsequent part of the previous symbol
 
-    private boolean isSubsequentSymbol(char c, StringBuffer previous) {
+    private static boolean isSubsequentSymbol(char c, StringBuffer previous) {
+        if (previous == null) {
+            return false;
+        }
         return switch (previous.toString()) {
             case "/", ">", "<", ":", "!" -> c == '=';
             case "&" -> c == '&';
@@ -115,5 +125,4 @@ public class Scanner {
             default -> false;
         };
     }
-
 }
