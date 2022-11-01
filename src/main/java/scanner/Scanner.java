70c4b12a77d24433e6e44044bf75c2c5f330749b
Yasmin <yasminhrdina@hotmail.com>
2022-11-01 17:28:57 +0100

    edited Scanner

--- src/main/java/scanner/Scanner.java
+++ src/main/java/scanner/Scanner.java
@@ -47,7 +47,7 @@ public class Scanner {
                         symAccu = new StringBuffer(1024);
                         symAccu.append(c);
                     } else if (Character.isWhitespace(c)) {
-                        // "".isBlank(); // ignore whitespace (stay in state 0 and do nothing)
+                        "".isBlank(); // ignore whitespace (stay in state 0 and do nothing)
                     } else {
                         throw new LexicalError("illegal char: " + c);
                     }
